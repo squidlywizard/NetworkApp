095be167e962fb14c04008613180da8c743ea248
squidlywizard <ztdenna@hotmail.com>
2019-01-04 08:36:30 -0700

    mucked about with appDelegate

--- NetworkApp/settings/AppDelegate.swift
+++ NetworkApp/settings/AppDelegate.swift
@@ -30,11 +30,7 @@ import UIKit
             
             
             func applicationWillResignActive(_ application: UIApplication) {
-                // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-                // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-                func applicationWillResignActive(_ application: UIApplication) {
-                    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-                    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
                 }
                 
                 func applicationDidEnterBackground(_ application: UIApplication) {
@@ -69,5 +65,5 @@ import UIKit
                         return false
 }
 
-    }}
+    }
 
